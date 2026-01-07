60fff52d4c8c243eb059e587b77ce24b78bf9ed1
Venusia <venusiabiruk16@gmail.com>
2026-01-07 06:19:52 -0800

    Added IMaterialRepository interface

--- src/SmartUniversity/Modules/contents/Domain/Repositories/IMaterialRepository.cs
+++ src/SmartUniversity/Modules/contents/Domain/Repositories/IMaterialRepository.cs
@@ -8,15 +8,15 @@ public interface IMaterialRepository
     Task<Material?> GetByIdAsync(Guid materialId);
     
     // Supports filtering by Lesson, Type, and Search + Pagination
-//     Task<(IEnumerable<Material> Items, int TotalCount)> GetByCourseIdAsync(
-//         Guid courseId, 
-//         Guid? lessonId, 
-//         string? fileType, 
-//         string? search, 
-//         string? sort, 
-//         int page, 
-//         int pageSize);
+    Task<(IEnumerable<Material> Items, int TotalCount)> GetByCourseIdAsync(
+        Guid courseId, 
+        Guid? lessonId, 
+        string? fileType, 
+        string? search, 
+        string? sort, 
+        int page, 
+        int pageSize);
         
-//     Task UpdateAsync(Material material);
-//     Task DeleteAsync(Material material);
-// }
+    Task UpdateAsync(Material material);
+    Task DeleteAsync(Material material);
+}
