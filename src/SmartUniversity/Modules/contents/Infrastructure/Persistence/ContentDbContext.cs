4f39cad3338cdb238eb4b52f135ddf213959fa47
Venusia <venusiabiruk16@gmail.com>
2026-01-07 06:32:59 -0800

    Configured ContentDbContext

--- src/SmartUniversity/Modules/contents/Infrastructure/Persistence/ContentDbContext.cs
+++ src/SmartUniversity/Modules/contents/Infrastructure/Persistence/ContentDbContext.cs
@@ -3,23 +3,23 @@ using SmartUniversity.Modules.Content.Domain.Entities;
 
 namespace SmartUniversity.Modules.Content.Infrastructure.Persistence;
 
-// public class ContentDbContext : DbContext
-// {
-//     public ContentDbContext(DbContextOptions<ContentDbContext> options)
-//         : base(options) { }
+public class ContentDbContext : DbContext
+{
+    public ContentDbContext(DbContextOptions<ContentDbContext> options)
+        : base(options) { }
 
-//     public DbSet<Material> Materials => Set<Material>();
+    public DbSet<Material> Materials => Set<Material>();
 
-//     protected override void OnModelCreating(ModelBuilder modelBuilder)
-//     {
-//         modelBuilder.Entity<Material>(entity =>
-//         {
-//             entity.ToTable("materials", "content");
-//             entity.HasKey(x => x.Id);
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Material>(entity =>
+        {
+            entity.ToTable("materials", "content");
+            entity.HasKey(x => x.Id);
 
-//             entity.Property(x => x.FileName).IsRequired();
-//             entity.Property(x => x.FilePath).IsRequired();
-//             entity.Property(x => x.FileType).IsRequired();
-//         });
-//     }
-// }
+            entity.Property(x => x.FileName).IsRequired();
+            entity.Property(x => x.FilePath).IsRequired();
+            entity.Property(x => x.FileType).IsRequired();
+        });
+    }
+}
