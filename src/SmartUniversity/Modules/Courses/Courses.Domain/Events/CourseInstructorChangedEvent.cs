12961bfd4c183d903289b15c2374d33e21ff6e4d
TsiOn <tshimelis23@gmail.com>
2026-01-08 14:01:35 +0300

    refactor(Courses.Domain): update CourseInstructorChangedEvent

--- src/SmartUniversity/Modules/Courses/Courses.Domain/Events/CourseInstructorChangedEvent.cs
+++ src/SmartUniversity/Modules/Courses/Courses.Domain/Events/CourseInstructorChangedEvent.cs
@@ -1,6 +1,11 @@
+using SmartUniversity.Modules.Enrollment.Domain.Events;
+
 namespace SmartUniversity.Modules.Courses.Domain.Events;
 
 public record CourseInstructorChangedEvent(
     Guid CourseId,
     Guid NewInstructorId
-);
+) : IDomainEvent
+{
+    public DateTime OccurredOn { get; } = DateTime.UtcNow;
+}
