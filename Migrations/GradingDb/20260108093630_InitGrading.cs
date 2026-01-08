901cf6caed0d183a90e21dba3843e257eb6af170
TsiOn <tshimelis23@gmail.com>
2026-01-08 12:46:43 +0300

    feat(Courses.Application): add CreateLessonCommand

---
+++ Migrations/GradingDb/20260108093630_InitGrading.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartUniversity.Migrations.GradingDb
+{
+    /// <inheritdoc />
+    public partial class InitGrading : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
