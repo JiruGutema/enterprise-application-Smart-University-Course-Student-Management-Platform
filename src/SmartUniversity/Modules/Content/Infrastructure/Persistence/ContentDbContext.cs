f68c81bfacbf50f5a6191f07a1ddc51108b75dd4
Venusia <venusiabiruk16@gmail.com>
2026-01-09 02:50:37 -0800

    ContentDbContext

--- src/SmartUniversity/Modules/Content/Infrastructure/Persistence/ContentDbContext.cs
+++ src/SmartUniversity/Modules/Content/Infrastructure/Persistence/ContentDbContext.cs
@@ -1,39 +1,3 @@
-// using Microsoft.EntityFrameworkCore;
-// using SmartUniversity.Modules.Content.Domain.Aggregates;
-// using SmartUniversity.Modules.Content.Infrastructure.Outbox;
-
-// namespace SmartUniversity.Modules.Content.Infrastructure.Persistence;
-
-// public class ContentDbContext : DbContext
-// {
-//     public ContentDbContext(DbContextOptions<ContentDbContext> options)
-//         : base(options) { }
-
-//     public DbSet<Material> Materials => Set<Material>();
-//     public DbSet<OutboxMessage> OutboxMessages => Set<OutboxMessage>();
-
-//     protected override void OnModelCreating(ModelBuilder modelBuilder)
-//     {
-//         modelBuilder.Entity<Material>(entity =>
-//         {
-//             entity.ToTable("materials", "content");
-//             entity.HasKey(x => x.Id);
-
-//             entity.Property(x => x.FileName).IsRequired();
-//             entity.Property(x => x.FilePath).IsRequired();
-//             entity.Property(x => x.FileType).IsRequired();
-//         });
-
-//         modelBuilder.Entity<OutboxMessage>(entity =>
-//         {
-//             entity.ToTable("outbox_messages", "content");
-//             entity.HasKey(x => x.Id);
-            
-//             entity.Property(x => x.Type).IsRequired();
-//             entity.Property(x => x.Content).IsRequired();
-//         });
-//     }
-// }
 using Microsoft.EntityFrameworkCore;
 using SmartUniversity.Modules.Content.Domain.Aggregates;
 using SmartUniversity.Modules.Content.Infrastructure.Outbox;
@@ -45,9 +9,8 @@ public class ContentDbContext : DbContext
     public ContentDbContext(DbContextOptions<ContentDbContext> options)
         : base(options) { }
 
-    // ✅ Real change: made DbSets writable and initialized to null!
-    public DbSet<Material> Materials { get; set; } = null!;
-    public DbSet<OutboxMessage> OutboxMessages { get; set; } = null!;
+    public DbSet<Material> Materials => Set<Material>();
+    public DbSet<OutboxMessage> OutboxMessages => Set<OutboxMessage>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -65,9 +28,10 @@ public class ContentDbContext : DbContext
         {
             entity.ToTable("outbox_messages", "content");
             entity.HasKey(x => x.Id);
-
+            
             entity.Property(x => x.Type).IsRequired();
             entity.Property(x => x.Content).IsRequired();
         });
     }
 }
+
